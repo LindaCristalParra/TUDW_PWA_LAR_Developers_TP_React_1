a84f966cd23a3dd9402282465ebd705216dc33c0
Ramir0Navarrete <39681359rr@gmail.com>
2026-04-11 20:57:19 -0300

    - Create Modal Component and style

---
+++ REACT-BUSTER/src/Components/Modal/Modal.jsx
@@ -0,0 +1,16 @@
+import styles from './Modal.module.css';
+
+export const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className={styles.overlay} onClick={onClose}>
+      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+        <button className={styles.closeButton} onClick={onClose}>
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
