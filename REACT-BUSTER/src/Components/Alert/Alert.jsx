25fe357bb5eb47b4b391bc33237c8d6c36c922e9
Ramir0Navarrete <39681359rr@gmail.com>
2026-04-13 17:45:31 -0300

    - fix add dynamic aria-role to Alert component

--- REACT-BUSTER/src/Components/Alert/Alert.jsx
+++ REACT-BUSTER/src/Components/Alert/Alert.jsx
@@ -1,9 +1,10 @@
 import styles from './Alert.module.css';
 
 export const Alert = ({ text, type = 'info' }) => {
-  
+  const ariaRole = type === 'danger' ? 'alert' : 'status';
+
   return (
-    <div className={`${styles.alertBox} ${styles[type]}`}>
+    <div className={`${styles.alertBox} ${styles[type]}`} role={ariaRole}>
       {text}
     </div>
   );
