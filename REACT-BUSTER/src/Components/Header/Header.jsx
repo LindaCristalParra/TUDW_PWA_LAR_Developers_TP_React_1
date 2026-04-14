d49db1a2b50f054fd40b365edd835da1472c0837
Linda Parra <linda.parra@est.fi.uncoma.edu.ar>
2026-04-14 20:07:08 -0300

    - Incorporation of a search component into a header and creation of JSON with movies

--- REACT-BUSTER/src/Components/Header/Header.jsx
+++ REACT-BUSTER/src/Components/Header/Header.jsx
@@ -1,32 +1,29 @@
-import Filter from '../Filter/Filter'; 
+import Filter from '../Filter/Filter';
 import SearchContainer from '../Search/SearchContainer';
 import styles from './Header.module.css';
 
 const Header = ({ currentFilters, onFilterChange, onSearch }) => {
   return (
     <header className={styles.headerContainer}>
-      
-     
+
+
       <div className={styles.logoContainer}>
         <h1 className={styles.logo}>
           REACT<br />BUSTER
         </h1>
       </div>
 
-      
+
       <div className={styles.navContainer}>
-        <Filter 
-          currentFilters={currentFilters} 
-          onFilterChange={onFilterChange} 
+        <Filter
+          currentFilters={currentFilters}
+          onFilterChange={onFilterChange}
         />
       </div>
 
-      
+
       <div className={styles.searchContainer}>
-        {/* <Search onSearch={onSearch}/> */}
-        <div className={styles.searchPlaceholder}>
-        <p>Buscador en desarrollo...</p>
-        </div>
+        <SearchContainer onSearchChange={onSearch} />
       </div>
 
     </header>
