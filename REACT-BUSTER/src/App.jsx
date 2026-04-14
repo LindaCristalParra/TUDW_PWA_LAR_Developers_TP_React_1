567d0a60f42377056b32fc60bcb722545c3e8b3b
Linda Parra <linda.parra@est.fi.uncoma.edu.ar>
2026-04-13 21:42:15 -0300

    - Creation of search folder and jsx and css files

--- REACT-BUSTER/src/App.jsx
+++ REACT-BUSTER/src/App.jsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
 
+import Search from "./Components/Search/Search";
+
+const movies = [
+  {
+    title: 'Buscando a Nemo',
+    image: '',
+    director: 'Andrew Stanton',
+    year: '2003',
+    genre: 'Infantil',
+    type: 'pelicula',
+    rating: ''
+  },
+  {
+    title: 'El Rey León',
+    image: '',
+    director: 'Roger Allers',
+    year: '1994',
+    genre: 'Infantil',
+    type: 'pelicula',
+    rating: ''
+  },
+  {
+    title: '',
+    image: '',
+    director: '',
+    year: '',
+    genre: '',
+    type: 'pelicula',
+    rating: ''
+  },
+];
 function App() {
 
+  const [searchValue, setSearchValue] = useState("");
+
+
   return (
-  <div>Hola React buster</div>
+    <div className="App">
+      <div>Hola React Buster</div>
+      <Search>
+        <input type="text" placeholder="Buscar por nombre o director" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+      </Search>
+    </div>
   )
 }
 
